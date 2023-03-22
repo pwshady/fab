8b84e0dedb88e8dd37cd892080d2bde8b9ebf853
Pwshady <pwshady@gmail.com>
2023-03-22 22:57:54 +0300

    a-b

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '05ae0fe80f2d537b87e11015a08ffe9c7808b4f4',
+        'name' => 'fa/fa',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fa/fa' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '05ae0fe80f2d537b87e11015a08ffe9c7808b4f4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
